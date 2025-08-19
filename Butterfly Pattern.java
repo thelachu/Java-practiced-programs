52bba81011f31a23bc6de2e1ca2d3a979a7eaaa1
thelachu <lachureddykummetha@gmail.com>
2025-08-19 14:04:10 +0530

    Add files via upload
    Butterfly Pattern using Java

---
+++ Butterfly Pattern.java
@@ -0,0 +1,36 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int n=5;
+        for (int i=1;i<=n;i++){
+
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+
+            } for(int k=1;k<=(2*(n-i));k++) {
+                System.out.print(" ");
+
+            }for(int l=1;l<=i;l++) {
+                System.out.print("*");
+
+            }                System.out.println("");
+
+
+        }
+        for (int i=4;i>=0;i--){
+
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+
+            } for(int k=1;k<=(2*(n-i));k++) {
+                System.out.print(" ");
+
+            }for(int l=1;l<=i;l++) {
+                System.out.print("*");
+
+            }                System.out.println("");
+
+
+        }
+    }
+}
